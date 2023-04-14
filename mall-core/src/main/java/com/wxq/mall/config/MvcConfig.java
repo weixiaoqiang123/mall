4b05d5bc282c8296a02e2f603ac1c98debf62bf8
weixiaoqiang <weixq5@asiainfo.com>
2023-04-15 00:08:37 +0800

    系统/调整系统配置类路径

--- mall-core/src/main/java/com/wxq/mall/config/MvcConfig.java
+++ mall-core/src/main/java/com/wxq/mall/config/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.wxq.mall.config;
 
+import com.wxq.mall.system.MallProperties;
 import com.wxq.mall.system.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
---
+++ mall-core/src/main/java/com/wxq/mall/config/SystemConfig.java
@@ -0,0 +1,12 @@
+package com.wxq.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author weixiaoqiang
+ * @date 2023/4/14
+ **/
+@Configuration
+public class SystemConfig {
+
+}
