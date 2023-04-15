d95e4fc366f7c5203e1fafbef4545c85f056f887
weixiaoqiang <weixq5@asiainfo.com>
2023-04-15 18:38:00 +0800

    系统/商品/补充商品轮播图文档

--- mall-core/src/main/java/com/wxq/mall/controller/PmsBannerController.java
+++ mall-core/src/main/java/com/wxq/mall/controller/PmsBannerController.java
@@ -1,11 +1,10 @@
 package com.wxq.mall.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import com.wxq.mall.model.PmsBanner;
 import com.wxq.mall.service.IPmsBannerService;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import java.util.Map;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import com.wxq.common.model.ResultBody;
@@ -16,6 +15,7 @@ import com.wxq.common.model.ResultBody;
  */
 @Controller
 @RequestMapping("/pms-banner")
+@ApiOperation("轮播图管理")
 public class PmsBannerController {
 
     @Resource
@@ -23,6 +23,7 @@ public class PmsBannerController {
 
     @PostMapping
     @ResponseBody
+    @ApiOperation("新增轮播图图片")
     public ResultBody add(@RequestBody PmsBanner pmsBanner) {
         pmsBannerService.add(pmsBanner);
         return ResultBody.success();
@@ -30,6 +31,7 @@ public class PmsBannerController {
 
     @PutMapping
     @ResponseBody
+    @ApiOperation("修改轮播图图片")
     public ResultBody update(@RequestBody PmsBanner pmsBanner) {
         pmsBannerService.update(pmsBanner);
         return ResultBody.success();
@@ -37,6 +39,7 @@ public class PmsBannerController {
 
     @DeleteMapping("/{id}")
     @ResponseBody
+    @ApiOperation("删除轮播图图片")
     public ResultBody delete(@PathVariable String id) {
         pmsBannerService.delete(id);
         return ResultBody.success();
@@ -44,22 +47,15 @@ public class PmsBannerController {
 
     @GetMapping("/{id}")
     @ResponseBody
+    @ApiOperation("根据ID查询轮播图图片")
     public ResultBody get(@PathVariable String id) {
         PmsBanner entity = pmsBannerService.get(id);
         return ResultBody.success(entity);
     }
 
-    @GetMapping("/findByPage")
-    @ResponseBody
-    public ResultBody findByPage(@RequestParam Map<String,Object> params,
-                                         @RequestParam Integer page,
-                                         @RequestParam Integer size) {
-        Page<PmsBanner> pageModel = pmsBannerService.findByPage(params, page, size);
-        return ResultBody.success(pageModel);
-    }
-
     @GetMapping("/findAll")
     @ResponseBody
+    @ApiOperation("查询轮播图列表")
     public ResultBody findAll() {
         List<PmsBanner> data = pmsBannerService.findAll();
         return ResultBody.success(data);
