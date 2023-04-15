0f513901599557d5da3aec6ae58608a2efa6e570
weixiaoqiang <weixq5@asiainfo.com>
2023-04-15 10:39:06 +0800

    系统/菜单/补充菜单按钮字典接口文档

--- mall-core/src/main/java/com/wxq/mall/controller/CmsMenuButtonDicController.java
+++ mall-core/src/main/java/com/wxq/mall/controller/CmsMenuButtonDicController.java
@@ -1,6 +1,7 @@
 package com.wxq.mall.controller;
 
 import com.wxq.mall.type.ButtonAuthType;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
@@ -22,6 +23,7 @@ import com.wxq.common.model.ResultBody;
  */
 @Controller
 @RequestMapping("/menu-button-dic")
+@Api(tags = "菜单按钮字典管理")
 public class CmsMenuButtonDicController {
 
     @Resource
@@ -29,6 +31,7 @@ public class CmsMenuButtonDicController {
 
     @PostMapping
     @ResponseBody
+    @ApiOperation("新增按钮字典")
     public ResultBody add(@RequestBody CmsMenuButtonDic cmsMenuButtonDic) {
         cmsMenuButtonDicService.add(cmsMenuButtonDic);
         return ResultBody.success();
@@ -36,6 +39,7 @@ public class CmsMenuButtonDicController {
 
     @PutMapping
     @ResponseBody
+    @ApiOperation("修改按钮字典")
     public ResultBody update(@RequestBody CmsMenuButtonDic cmsMenuButtonDic) {
         cmsMenuButtonDicService.update(cmsMenuButtonDic);
         return ResultBody.success();
@@ -43,20 +47,15 @@ public class CmsMenuButtonDicController {
 
     @DeleteMapping("/{id}")
     @ResponseBody
+    @ApiOperation("删除按钮字典")
     public ResultBody delete(@PathVariable String id) {
         cmsMenuButtonDicService.delete(id);
         return ResultBody.success();
     }
 
-    @GetMapping("/{id}")
-    @ResponseBody
-    public ResultBody get(@PathVariable String id) {
-        CmsMenuButtonDic entity = cmsMenuButtonDicService.get(id);
-        return ResultBody.success(entity);
-    }
-
     @GetMapping("/findByPage")
     @ResponseBody
+    @ApiOperation("分页查询")
     public ResultBody findByPage(@RequestParam Map<String,Object> params,
                                          @RequestParam Integer page,
                                          @RequestParam Integer size) {
@@ -66,18 +65,20 @@ public class CmsMenuButtonDicController {
 
     @GetMapping("/findAllButtonAuthTypes")
     @ResponseBody
+    @ApiOperation("查询所有按钮权限类型")
     public ResultBody findAllButtonAuthTypes(){
         return ResultBody.success(Arrays.asList(ButtonAuthType.values()));
     }
 
     @GetMapping("/checkCurrentMenuButtonIdUnique")
     @ResponseBody
+    @ApiOperation("校验菜单按钮ID在当前菜单中是否唯一")
     public ResultBody checkCurrentMenuButtonIdUnique(@RequestParam String menuId, @RequestParam String buttonId){
         cmsMenuButtonDicService.checkCurrentMenuButtonIdUnique(menuId, buttonId);
         return ResultBody.success();
     }
 
-    @GetMapping("/checkCurrentMenuButtonIdUnique")
+    @GetMapping("/findMenuButtonsByMenuId")
     @ResponseBody
     @ApiOperation("根据菜单编码查询所有菜单按钮")
     public ResultBody findMenuButtonsByMenuId(String menuId) {
